8277b50591ea12c9d78d7d8b6d7fce7d35a96595
siluansf <siluansf@web.de>
2017-03-09 19:24:04 +0100

    matrix 19:23

--- mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
+++ mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
@@ -11,7 +11,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Hello world!");
         System.out.println("Hello world! Another one!");
+
     }
+    protected void CalculateTime(int hour1, int hour2, int minute1, int minute2){
+        int matrix[][] = {};
+        while(hour1 != 0){
+            matrix[3][0] = hour1 % 2;
+            hour1 /= 2;
+
+            matrix[2][0] = hour1 % 2;
+            hour1 /= 2;
+
+            matrix[1][0] = hour1 % 2;
+            hour1 /= 2;
+
+            matrix[0][0] = hour1 % 2;
+            hour1 /= 2;
+        }
+        while(hour2 != 0){
+            matrix[3][1] = hour1 % 2;
+            hour2 /= 2;
+
+            matrix[2][1] = hour1 % 2;
+            hour2 /= 2;
+
+            matrix[1][1] = hour1 % 2;
+            hour2 /= 2;
+
+            matrix[0][1] = hour1 % 2;
+            hour2 /= 2;
+        }
+        while(minute1 != 0){
+            matrix[3][2] = hour1 % 2;
+            minute1 /= 2;
+
+            matrix[2][2] = hour1 % 2;
+            minute1 /= 2;
+
+            matrix[1][2] = hour1 % 2;
+            minute1 /= 2;
+
+            matrix[0][2] = hour1 % 2;
+            minute1 /= 2;
+        }
+        while(minute2 != 0){
+            matrix[3][3] = hour1 % 2;
+            minute2 /= 2;
+
+            matrix[2][3] = hour1 % 2;
+            minute2 /= 2;
+
+            matrix[1][3] = hour1 % 2;
+            minute2 /= 2;
+
+            matrix[0][3] = hour1 % 2;
+            minute2 /= 2;
+        }
+    }
+
 }
 
 
