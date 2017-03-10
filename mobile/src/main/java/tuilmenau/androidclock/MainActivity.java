9af3dec6fbe00a7f95b207494927abbf401c7323
HeLau1337 <Hendrik.Laudemann+git@gmail.com>
2017-03-10 18:41:11 +0100

    Hello worlds removed 18:40

--- mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
+++ mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
@@ -9,25 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello world!");
-        System.out.println("Hello world! Another one!");
-<<<<<<< HEAD
-<<<<<<< HEAD
-        System.out.println("Hello develop!");
-        System.out.println("Hello commit!");
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 6668545135c6b4c72d9a5b0971928aeddab95f76
-
-
-        System.out.println("Hello world! Are you ready to code?!");
-<<<<<<< HEAD
->>>>>>> 0499f7ecdcf8beda1a46fcf8f695330a3ddfc396
->>>>>>> 9a46e7b5030d1e9faaf630ba3b1c07c650d6c2b7
-=======
-
->>>>>>> 6668545135c6b4c72d9a5b0971928aeddab95f76
+        //"Hello world"s removed
     }
     protected void CalculateTime(int hour1, int hour2, int minute1, int minute2){
         int matrix[][] = {};
