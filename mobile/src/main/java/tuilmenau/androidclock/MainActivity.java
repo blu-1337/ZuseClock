3a73f7abfa54422b449bf416f1bd09edc19c7d9f
HeLau1337 <Hendrik.Laudemann+git@gmail.com>
2017-03-11 23:14:28 +0100

    design improvements (not in upper left corner anymore) and get time from Calendar (does not work perfectly yet)

--- mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
+++ mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
@@ -21,18 +21,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //example: make imageview (in)visible
+        //######### example: make imageview (in)visible
         ImageView hour00 = (ImageView) findViewById(R.id.imageView1);
         hour00.setVisibility(View.VISIBLE);
         ImageView hour10 = (ImageView) findViewById(R.id.imageView2);
         hour10.setVisibility(View.VISIBLE);
 
-        //test: read system time (hour + minutes)
-        Calendar calendar = Calendar.getInstance();
-        int current_hour = calendar.get(Calendar.HOUR);
+        //######### test: read system time (hour + minutes)############
+
+        //new Time(System.currentTimeMillis()).getHours();
+        //Calendar calendar = calendar.getInstance();
+        //long timenow = System.currentTimeMillis();
+        int current_hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);//int current_hour = calendar.get(GregorianCalendar.HOUR_OF_DAY);
+        int current_minutes = Calendar.getInstance().get(Calendar.MINUTE);
 
         TextView current_time = (TextView) findViewById(R.id.textView1);
-        current_time.setText(hour1(current_hour) + " " + hour2(current_hour));
+        current_time.setText(hour1(current_hour) + "     " + hour2(current_hour) + "                        " + minute1(current_minutes) + "     " + minute2(current_minutes));
+
+        TextView time_now = (TextView) findViewById(R.id.textView2);
+        time_now.setText("hi");
 
     }
 
@@ -42,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
 
     public int hour2(int current_hour) {
         return (int) ((current_hour / Math.pow(10, 2)) % 10);
+    }
+
+    public int minute1(int current_minutes) {
+        return (int) ((current_minutes / Math.pow(10, 1)) % 10);
+    }
+
+    public int minute2(int current_minutes) {
+        return (int) ((current_minutes / Math.pow(10, 2)) % 10);
     }
 
 
