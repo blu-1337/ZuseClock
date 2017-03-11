f2ae3ad8c40281a1a02fe21fc5551575362112cf
siluansf <siluansf@web.de>
2017-03-11 19:40:25 +0100

    commites changes 19:40

--- mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
+++ mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
@@ -11,66 +11,65 @@ public class MainActivity extends AppCompatActivity {
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CalculateTime(2,0,5,3);
-    }
-    protected void CalculateTime(int hour1, int hour2, int minute1, int minute2){
-        int matrix[][] = {};
+//        CalculateTime(2,0,5,3);
+//    }
+//    protected void CalculateTime(int hour1, int hour2, int minute1, int minute2){
+//        int matrix[][] = {};
+//
+//        //first column
+//            matrix[3][0] = hour1 % 2;
+//            hour1 /= 2;
+//
+//            matrix[2][0] = hour1 % 2;
+//            hour1 /= 2;
+//
+//            matrix[1][0] = hour1 % 2;
+//            hour1 /= 2;
+//
+//            matrix[0][0] = hour1 % 2;
+//
+//
+//        //second column
+//            matrix[3][1] = hour1 % 2;
+//            hour2 /= 2;
+//
+//            matrix[2][1] = hour1 % 2;
+//            hour2 /= 2;
+//
+//            matrix[1][1] = hour1 % 2;
+//            hour2 /= 2;
+//
+//            matrix[0][1] = hour1 % 2;
+//
+//        //third column
+//            matrix[3][2] = hour1 % 2;
+//            minute1 /= 2;
+//
+//            matrix[2][2] = hour1 % 2;
+//            minute1 /= 2;
+//
+//            matrix[1][2] = hour1 % 2;
+//            minute1 /= 2;
+//
+//            matrix[0][2] = hour1 % 2;
+//
+//        //fourth column
+//            matrix[3][3] = hour1 % 2;
+//            minute2 /= 2;
+//
+//            matrix[2][3] = hour1 % 2;
+//            minute2 /= 2;
+//
+//            matrix[1][3] = hour1 % 2;
+//            minute2 /= 2;
+//
+//            matrix[0][3] = hour1 % 2;
+//
+//        ImageView hour00 = (ImageView) findViewById(R.id.imageView1);
+//                hour00.setVisibility(1);
+//        ImageView hour10 = (ImageView) findViewById(R.id.imageView2);
+//                hour10.setVisibility(0);
+//    }
 
-        //first column
-            matrix[3][0] = hour1 % 2;
-            hour1 /= 2;
-
-            matrix[2][0] = hour1 % 2;
-            hour1 /= 2;
-
-            matrix[1][0] = hour1 % 2;
-            hour1 /= 2;
-
-            matrix[0][0] = hour1 % 2;
-
-
-        //second column
-            matrix[3][1] = hour1 % 2;
-            hour2 /= 2;
-
-            matrix[2][1] = hour1 % 2;
-            hour2 /= 2;
-
-            matrix[1][1] = hour1 % 2;
-            hour2 /= 2;
-
-            matrix[0][1] = hour1 % 2;
-
-        //third column
-            matrix[3][2] = hour1 % 2;
-            minute1 /= 2;
-
-            matrix[2][2] = hour1 % 2;
-            minute1 /= 2;
-
-            matrix[1][2] = hour1 % 2;
-            minute1 /= 2;
-
-            matrix[0][2] = hour1 % 2;
-
-        //fourth column
-            matrix[3][3] = hour1 % 2;
-            minute2 /= 2;
-
-            matrix[2][3] = hour1 % 2;
-            minute2 /= 2;
-
-            matrix[1][3] = hour1 % 2;
-            minute2 /= 2;
-
-            matrix[0][3] = hour1 % 2;
-
-        ImageView hour00 = (ImageView) findViewById(R.id.imageView1);
-                hour00.setVisibility(1);
-        ImageView hour10 = (ImageView) findViewById(R.id.imageView2);
-                hour10.setVisibility(0);
-    }
-
-}
 
 
