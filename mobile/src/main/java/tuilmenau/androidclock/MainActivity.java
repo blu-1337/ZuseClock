c73ca808cc82979b8cca5c65af85a42729dae61c
HeLau1337 <Hendrik.Laudemann+git@gmail.com>
2017-03-12 18:15:52 +0100

    removed unnecessary comments

--- mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
+++ mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
@@ -29,16 +29,12 @@ public class MainActivity extends AppCompatActivity {
         hour10.setVisibility(View.VISIBLE);
 
         //######### test: read system time (hour + minutes)############
-
-        //new Time(System.currentTimeMillis()).getHours();
         //Calendar calendar = calendar.getInstance();
-        //long timenow = System.currentTimeMillis();
         int current_hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);//int current_hour = calendar.get(GregorianCalendar.HOUR_OF_DAY);
         int current_minutes = Calendar.getInstance().get(Calendar.MINUTE);
 
         TextView current_time = (TextView) findViewById(R.id.textView1);
-        current_time.setText(" ");
-        current_time.setText(current_hour + " " + hour1(current_hour) + "     " + hour2(current_hour) + "                " + minute1(current_minutes) + "     " + minute2(current_minutes));
+        current_time.setText(hour1(current_hour) + "     " + hour2(current_hour) + "                " + minute1(current_minutes) + "     " + minute2(current_minutes));
 
         TextView time_now = (TextView) findViewById(R.id.textView2);
         time_now.setText("hi");
@@ -54,16 +50,6 @@ public class MainActivity extends AppCompatActivity {
         }
         int digit = array[1];
         return digit;
-        /*int digit, rn;
-        do{
-            digit = x % 10;
-            rn = x / 10;
-
-            *//*x /= 10;
-            int digit = x % 10;
-            return digit;*//*
-        } while(rn > 0);
-        return digit;*/
     }
 
     public int hour2(int x) {
