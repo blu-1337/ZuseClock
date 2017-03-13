f6e98b50dc9d16341ae7aa557affe3b13c23532d
siluansf <siluansf@web.de>
2017-03-13 19:21:57 +0100

    saves current state

--- mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
+++ mobile/src/main/java/tuilmenau/androidclock/MainActivity.java
@@ -61,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         MainActivity test = new MainActivity();
         test.start();
 
